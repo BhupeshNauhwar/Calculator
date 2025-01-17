3121d68a466e721dc12c41aab171a620da1e49e3
Bhupesh Kumar <bhupeshkumar052000@gmail.com>
2025-01-17 14:33:58 +0530

    div

---
+++ Division.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Division {
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter 2 Numbers ");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        
+
+        System.out.println("Division of three numbers are "+ (a/b));
+    }
+}
